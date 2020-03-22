65be3dc3fed0027bdb1163b08c1a056a9018319f
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-22 02:29:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,27 +1,45 @@
-require 'pry'
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# INPUT --> hash {box lables => box sizes}
+# OUTPUT --> Number, which is the smallest value in the input hash
+
+# open each door and check the box sizes. keep track of the smallest box that I have seen
+# return the label of the box with the smallest size
+
+# -----------------------------------------------------------
+
+# SEUDOCODE :
+
+# smallest box label, smalles box size = nil
+# for each key,value pair
+#   compare value to smallest box size
+#   if value is less than smallest box size
+#       set smallest box size to value
+#       set smallest box label to key
+#   end
+# END EACH
+# return smallest box label
 
 def key_for_min_value(name_hash)
-smallest_name = nil
-smallest_num = nil
-
-  name_hash.each do |name, num|
-    if smallest_num == nil || num < smallest_num
-      binding.pry
-      smallest_name = name
-      smallest_num = num
+  lowest_key = nil #smallest Key value we know of
+  lowest_val = nil #smallest Value, value we know of
+    name_hash.each do |key, val|
+      if val < lowest_val
+        lowest_key = key
+        lowest_val = val
+      end
     end
-  end
-  return smallest_name
+    return lowest_key
 
 end
 
 
+# -----------------------------------------------------------
+
+#COMPLETEED WITH Float::INFINITY :
+
 # lowest_key = nil
 # lowest_val = Float::INFINITY
 
-#   hash.each do |key, val|
+#   name_hash.each do |key, val|
 #     if val < lowest_val
 #       lowest_val = val
 #       lowest_key = key
